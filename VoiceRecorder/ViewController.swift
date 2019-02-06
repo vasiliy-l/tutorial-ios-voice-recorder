f32e04303d2c4e46e2ab3eff34af00ef421baa07
Vasiliy Lada <vasiliy.lada@protonmail.com>
2019-02-06 21:49:43 +0200

    Initial Commit

---
+++ VoiceRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceRecorder
+//
+//  Created by  William on 2/6/19.
+//  Copyright © 2019 Vasiliy Lada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
